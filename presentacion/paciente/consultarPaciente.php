1172128de823644f7f0dcc985f02daf168dc424d
Oscar Cely <oscarcej@gmail.com>
2020-02-10 22:19:16 -0500

    The fckng AJAX is finally Working

--- presentacion/paciente/consultarPaciente.php
+++ presentacion/paciente/consultarPaciente.php
@@ -31,7 +31,7 @@ include 'presentacion/menuAdministrador.php';
 								echo "<td>" . $p->getNombre() . "</td>";
 								echo "<td>" . $p->getApellido() . "</td>";
 								echo "<td>" . $p->getCorreo() . "</td>";
-								echo "<td>" . (($p->getEstado()==1)?"<i class='fas fa-check-circle fa-2x text-success'></i>":"<i class='fas fa-times-circle fa-2x text-danger'></i>") . "</td>";
+								echo "<td> <div id='est" . $p->getId() . "'>" . (($p->getEstado()==1)?"<i class='fas fa-check-circle fa-2x text-success'></i>":"<i class='fas fa-times-circle fa-2x text-danger'></i>") . "</td>";
 								echo "<td>" . (($p->getFoto() !== "" && file_exists("img/" . $p->getFoto() . "") && $p->getFoto()) ?
 									"<img src='img/" . $p->getFoto() . "' alt='Imagen de usuario" . $p->getFoto() . "' height='50px'>" : "<i class='fas fa-user-tie fa-3x'></i>") . "</td>";
 
@@ -44,9 +44,12 @@ include 'presentacion/menuAdministrador.php';
 					   			<a class='fas fa-camera' href='index.php?pid=" . base64_encode("presentacion/paciente/actualizarFotoPaciente.php") .
 									"&idPaciente=" . $p->getId() . "' data-toggle='tooltip' data-placement='bottom' title='Actualizar Foto'></a>";
 									  
-								echo ($p->getEstado())?"<a class='fas fa-lock-open' id='hab" .$p->getId() ."' href='#' data-toggle='tooltip' data-placement='right' 
-								title='Inhabilitar paciente'></a>":"<a class='fas fa-lock' href='#' id='hab" .$p->getId() ."' data-toggle='tooltip' 
-								data-placement='right' title='Habilitar paciente'></a>" . "</td>";
+// 								echo ($p->getEstado())?"<span id='inhab" . $p->getId() . "'><a class='fas fa-lock-open' id='hab" .$p->getId() ."' href='#' data-toggle='tooltip' data-placement='right' 
+// 								title='Inhabilitar paciente'></a></span>":"<span id='hab" . $p->getId() . "'><a class='fas fa-lock' href='#' id='hab" .$p->getId() ."' data-toggle='tooltip' 
+// 								data-placement='right' title='Habilitar paciente'></a></span>" . "</td>";
+								
+								echo "<span id='status" . $p->getId() . "'><a style='margin-left: 3px' class='" . (($p->getEstado()==0)?"fas fa-lock-open' title='Habilitar paciente' ": 
+								"fas fa-lock' title='Inhabilitar paciente'") . "' id='hab" .$p->getId() . "' href='#' data-toggle='tooltip' data-placement='right' </a></span>";
 
 								echo "</tr>";
 
@@ -81,7 +84,7 @@ include 'presentacion/menuAdministrador.php';
 $(document).ready(function(){	
 	<?php foreach ($pacientes as $p) { ?>
 		$("#hab<?php echo $p -> getId();?>").click(function(){
-			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . (($p -> getEstado() == 1)?"1":"0") . "\";" ?>
+			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . $p -> getEstado() . "\";"; ?>
 			$("#hab<?php echo $p -> getId();?>").tooltip('hide');
 			$("#pac<?php echo $p -> getId();?>").load(ruta);
 		});
--- presentacion/paciente/editarEstadoPacienteAjax.php
+++ presentacion/paciente/editarEstadoPacienteAjax.php
@@ -2,35 +2,35 @@
 $paciente = new Paciente($_GET['idPaciente'], "", "", "", "", "", $_GET['estado']);
 $paciente->actualizarEstado();
 $paciente->consultar();
-echo "<td>" . $paciente->getId() . "</td>";
-echo "<td>" . $paciente->getNombre() . "</td>";
-echo "<td>" . $paciente->getApellido() . "</td>";
-echo "<td>" . $paciente->getCorreo() . "</td>";
-echo "<td>" . (($paciente->getEstado()==1)?"<i class='fas fa-check-circle fa-2x text-success'></i>":"<i class='fas fa-times-circle fa-2x text-danger'></i>") . "</td>";
-echo "<td>" . (($paciente->getFoto() !== "" && file_exists("img/" . $paciente->getFoto() . "") && $paciente->getFoto()) ?
-"<img src='img/" . $paciente->getFoto() . "' alt='Imagen de usuario" . $paciente->getFoto() . "' height='50px'>" : "<i class='fas fa-user-tie fa-3x'></i>") . "</td>";
+$p = $paciente;
+
+echo "<td>" . $p->getId() . "</td>";
+echo "<td>" . $p->getNombre() . "</td>";
+echo "<td>" . $p->getApellido() . "</td>";
+echo "<td>" . $p->getCorreo() . "</td>";
+echo "<td> <div id='est" . $p->getId() . "'>" . (($p->getEstado()==1)?"<i class='fas fa-check-circle fa-2x text-success'></i>":"<i class='fas fa-times-circle fa-2x text-danger'></i>") . "</td>";
+echo "<td>" . (($p->getFoto() !== "" && file_exists("img/" . $p->getFoto() . "") && $p->getFoto()) ?
+		"<img src='img/" . $p->getFoto() . "' alt='Imagen de usuario" . $p->getFoto() . "' height='50px'>" : "<i class='fas fa-user-tie fa-3x'></i>") . "</td>";
 
 echo "<td>" .
-"<a href='modalPaciente.php?idPaciente=" . $paciente->getId() . "' data-toggle='modal' data-target='#modalPaciente' ><span class='fas fa-eye' data-toggle='tooltip' class='tooltipLink' data-placement='left' data-original-title='Ver Detalles' ></span> </a>
-<a class='fas fa-pencil-ruler' href='index.php?pid=" .
-base64_encode("presentacion/paciente/actualizarPaciente.php") . "&idPaciente=" .
-$paciente->getId() . "' data-toggle='tooltip' data-placement='top' title='Actualizar'> </a>
-
-<a class='fas fa-camera' href='index.php?pid=" . base64_encode("presentacion/paciente/actualizarFotoPaciente.php") .
-"&idPaciente=" . $paciente->getId() . "' data-toggle='tooltip' data-placement='bottom' title='Actualizar Foto'></a>";
-
-echo ($paciente->getEstado())?"<a class='fas fa-lock-open' id='hab" .$paciente->getId() ."' href='#' data-toggle='tooltip' data-placement='right' 
-title='Inhabilitar paciente'></a>":"<a class='fas fa-lock' href='#' id='hab" .$paciente->getId() ."' data-toggle='tooltip' 
-data-placement='right' title='Habilitar paciente'></a>" . "</td>";
+		"<a href='modalPaciente.php?idPaciente=" . $p->getId() . "' data-toggle='modal' data-target='#modalPaciente' ><span class='fas fa-eye' data-toggle='tooltip' class='tooltipLink' data-placement='left' data-original-title='Ver Detalles' ></span> </a>
+								<a class='fas fa-pencil-ruler' href='index.php?pid=" .
+								base64_encode("presentacion/paciente/actualizarPaciente.php") . "&idPaciente=" .
+								$p->getId() . "' data-toggle='tooltip' data-placement='top' title='Actualizar'> </a>
+										
+					   			<a class='fas fa-camera' href='index.php?pid=" . base64_encode("presentacion/paciente/actualizarFotoPaciente.php") .
+					   			"&idPaciente=" . $p->getId() . "' data-toggle='tooltip' data-placement='bottom' title='Actualizar Foto'></a>";
+								
+								
+								echo "<span id='status" . $p->getId() . "'><a style='margin-left: 3px' class='" . (($p->getEstado()==0)?"fas fa-lock-open' title='Habilitar paciente' ":
+										"fas fa-lock' title='Inhabilitar paciente'") . "' id='hab" .$p->getId() . "' href='#' data-toggle='tooltip' data-placement='right' </a></span>";
+								echo "</td>"
 ?>
-
-
-<script type="text/javascript">
-$(document).ready(function(){	
-		$("#hab<?php echo $paciente -> getId();?>").click(function(){
-			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . (($paciente -> getEstado() == 1)?"1":"0") . "\";" ?>
-			$("#hab<?php echo $paciente -> getId();?>").tooltip('hide');
-			$("#pac<?php echo $paciente -> getId();?>").load(ruta);
-		});
-	});
-	</script>
+<script>
+$('[data-toggle="tooltip"]').tooltip();
+$("#hab<?php echo $p -> getId();?>").click(function(){
+	<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . $p -> getEstado() . "\";"; ?>
+	$("#hab<?php echo $p -> getId();?>").tooltip('hide');
+	$("#pac<?php echo $p -> getId();?>").load(ruta);
+});
+</script>
