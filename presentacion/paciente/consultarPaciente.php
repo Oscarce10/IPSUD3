ab3133292bc66b2bd3f6ca1e5a67aa2ec7f34c86
Oscar Cely <oscarcej@gmail.com>
2020-02-17 20:47:54 -0500

    Intentando hacer filtro de pacientes v0.00001

--- presentacion/paciente/consultarPaciente.php
+++ presentacion/paciente/consultarPaciente.php
@@ -1,51 +1,28 @@
 <?php
-$administrador = new Administrador($_SESSION['id']);
-$administrador->consultar();
-$paciente = new Paciente();
-//$pacientes = $paciente->consultarTodos();
-include 'presentacion/menuAdministrador.php';
+$administrador = new Administrador ( $_SESSION ['id'] );
+$administrador->consultar ();
 
+include 'presentacion/menuAdministrador.php';
 ?>
-<div class="container">
+
+
+<div class="container" style="margin-top: 20px;">
 	<div class="row">
-		<div  class="col-6">
+		<div class="col-6">
 			<!-- Search form -->
-			<form class="form-inline active-pink-3 active-pink-4" >
-				<i class="fas fa-search" aria-hidden="true"></i> 
-				<input
-					class="form-control form-control-sm ml-3 w-75" type="text" id="formConsulta"
+			<form class="form-inline active-pink-3 active-pink-4">
+				<i class="fas fa-search" aria-hidden="true"></i> <input
+					class="form-control form-control-sm ml-3 w-75" type="text"
+					id="formConsulta"
 					placeholder="Buscar paciente por nombre o apellido"
 					aria-label="Search">
 			</form>
 		</div>
 	</div>
 
-	<div class="col-12">
-		<div class="card">
-			<div class="card-header bg-primary text-white">Consultar Paciente</div>
-			<div class="card-body">
-				<table class="table table-striped table-hover">
-					<thead>
-						<tr>
-							<th scope="col">Id</th>
-							<th scope="col">Nombre</th>
-							<th scope="col">Apellido</th>
-							<th scope="col">Correo</th>
-							<th scope="col">Estado</th>
-							<th scope="col">Foto</th>
-							<th scope="col">Servicios</th>
-						</tr>
-					</thead>
-					<tbody class="resConsulta">
-							
-						</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
 
+<div class="col-12" id="tabla"></div>
+</div>
 
 
 <div class="modal fade" id="modalPaciente" role="dialog">
@@ -55,40 +32,24 @@ include 'presentacion/menuAdministrador.php';
 </div>
 
 <script>
+$(document).ready(function(){	
+	
 	$('body').on('show.bs.modal', '.modal', function (e) {
 		var link = $(e.relatedTarget);
 		$(this).find(".modal-content").load(link.attr("href"));
 	});
+
+	$("#formConsulta").keyup(function() {
+		console.log( $("#formConsulta").val());
+		if($("#formConsulta").val() != ""){
+			$("#tabla").show();
+			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/filtroPacientes.php")."\";";?>
+			$("#tabla").load(ruta, {"filtro": $("#formConsulta").val()})
+		}
+		else
+			$("#tabla").hide();
+		
+			});	
+	
+});
 </script>
-
-<script type="text/javascript">
-$(document).ready(function(){	
-	$( "#formConsulta" ).keyup(function() {
-		console.log($("#formConsulta").val());
-		$filtro = ($filtro==null)?$("#formConsulta").val();
-		$pacientes = null;
-		$pacientes = $paciente-- >filtroPaciente($filtro);
-		<?php 
-		if($pacientes!= null){
-		foreach ($pacientes as $p) { ?>
-		$("#hab<?php echo $p -> getId();?>").click(function(){
-			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . $p -> getEstado() . "\";"; ?>
-			// Esto esconde el Tooltip del candado previamente seleccionado
-			$("#hab<?php echo $p -> getId();?>").tooltip('hide');
-			// Esto carga toda la capa de la fila de la tabla del paciente a actualizar vease arriba que la etiqueta <tr> contiene el id pac#
-			$("#pac<?php echo $p -> getId();?>").load(ruta);
-		});
-		<?php }} ?>
-		});
-
-	<?php foreach ($pacientes as $p) { ?>
-		$("#hab<?php echo $p -> getId();?>").click(function(){
-			<?php echo "var ruta = \"indexAjax.php?pid=" . base64_encode("presentacion/paciente/editarEstadoPacienteAjax.php") . "&idPaciente=" . $p -> getId() . "&estado=" . $p -> getEstado() . "\";"; ?>
-			// Esto esconde el Tooltip del candado previamente seleccionado
-			$("#hab<?php echo $p -> getId();?>").tooltip('hide');
-			// Esto carga toda la capa de la fila de la tabla del paciente a actualizar vease arriba que la etiqueta <tr> contiene el id pac#
-			$("#pac<?php echo $p -> getId();?>").load(ruta);
-		});
-		<?php } ?>
-	});
-	</script>
