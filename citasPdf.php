7467d3917f7b1561923015f06a7b2ebde965d141
Oscar Cely <oscarcej@gmail.com>
2020-03-04 16:03:04 -0500

    Ya esta el pdf de citas

--- citasPdf.php
+++ citasPdf.php
@@ -34,26 +34,34 @@ class MyPdf extends FPDF{
 	function viewTable() {
 		$paciente = new Paciente();
 		$citas = $paciente->obtenerCitas();
-		foreach ( $citas as $p ) {
-			$this->SetFont('Lexend',"", 12);
-			$this->Cell(112.5 ,20 ,"Paciente: " . $p[1] , 1, 0);
-			$this->Cell(112.5 ,20 ,"Correo: " . $p[2] , 1, 0);
-			$this->Cell(50 ,20 , $this->Image(($p[3] != "" && file_exists ( "img/" . $p[2] . "" ) && $p[2])?"img/".$p[2]:"img/user.jpg", $this->GetX()+1, $this->GetY()+1,45, 16) , 1);
-			$this->Ln();
-			$this->Cell(40 ,10 ,"Fecha" , 1, 0, "C");
-			$this->Cell(40 ,10 ,"Hora" , 1, 0, "C");
-			$this->Cell(120 ,10 ,"Medico" , 1, 0, "C");
-			$this->Cell(35 ,10 ,"Consultorio" , 1, 0, "C");
-			$this->Cell(40 ,10 ,"Especialidad" , 1, 0, "C");
-			$this->Ln();
+		$pac = null;
+		foreach ( $citas as $p ) {			
+			if($pac == null || $pac != $p[0]){
+				if($pac != null)
+					$this->AddPage();
+				
+				$this->SetFont('Lexend',"", 12);
+				$this->Cell(112.5 ,20 ,"Paciente: " . $p[1] , 1, 0);
+				$this->Cell(112.5 ,20 ,"Correo: " . $p[2] , 1, 0);
+				$this->Cell(50 ,20 , $this->Image(($p[3] != "" && file_exists ( "img/" . $p[3] . "" ) && $p[2])?"img/".$p[3]:"img/user.jpg", $this->GetX()+1, $this->GetY()+1,45, 16) , 1);
+				$this->Ln();
+				$this->Cell(40 ,10 ,"Fecha" , 1, 0, "C");
+				$this->Cell(40 ,10 ,"Hora" , 1, 0, "C");
+				$this->Cell(100 ,10 ,"Medico" , 1, 0, "C");
+				$this->Cell(35 ,10 ,"Consultorio" , 1, 0, "C");
+				$this->Cell(60 ,10 ,"Especialidad" , 1, 0, "C");
+				$this->Ln();
+			}		
+			
 			$this->Cell(40 ,10 , $p[4] , 1, 0, "C");
 			$this->Cell(40 ,10 , $p[5] , 1, 0, "C");
-			$this->Cell(120 ,10 ,$p[6] , 1, 0, "C");
+			$this->Cell(100 ,10 ,$p[6] , 1, 0, "C");
 			$this->Cell(35 ,10 , $p[7] , 1, 0, "C");
-			$this->Cell(40 ,10 , $p[8] , 1, 0, "C");
+			$this->Cell(60 ,10 , $p[8] , 1, 0, "C");
 			$this->Ln();
+			$pac = $p[0];
 		}
-		$this->AddPage();
+		
 	}
 }
 
